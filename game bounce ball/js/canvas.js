2cd9a643de76874127878f9a5d4c44403ac88792
Green <imgreenwu@gamil.com>
2017-12-18 16:46:07 +0800

    1

--- game bounce ball/js/canvas.js
+++ game bounce ball/js/canvas.js
@@ -7,7 +7,21 @@ var log = console.log.bind(console);
 /**
  * --------------------------------------------------------------------
  */
-function pre_rect(){
+
+function preference () {
+    var a = {
+        x : 350, //初始位置
+        speed : 5, //移动速度
+        frameRate : 60, //帧率
+        rightBorder : 800, //右边界
+        leftBorder : 0, //左边界
+    }
+    return a;
+}
+
+var _settings = preference();
+
+ function pre_draw(){
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#e98c8c";
@@ -17,15 +31,10 @@ function pre_rect(){
     }
 }
 
-var rect = pre_rect();
-window.onload = rect(350);
+var draw = pre_draw();
+window.onload = draw(_settings.x);
 
 function pre_move () {
-    var x = 350; //初始位置
-    var speed = 5; //移动速度
-    var frameRate = 60; //帧率
-    var rightBorder = 800; //右边界
-    var leftBorder = 0; //左边界
     var timerR,timerL; //定时器
     return function (e) {
         var key = e.key;
@@ -33,18 +42,18 @@ function pre_move () {
         clearInterval(timerL);
         if (key === "ArrowRight") {
             timerR = setInterval(function(){
-                if (x <= rightBorder - 105) {
-                    x += speed;
-                    rect(x);
+                if (_settings.x <= _settings.rightBorder - 105) {
+                    _settings.x += _settings.speed;
+                    draw(_settings.x);
                 }
-            },1000/frameRate)
+            },1000/_settings.frameRate)
         } else if (key === "ArrowLeft") {
             timerL = setInterval(function(){
-                if (x >= leftBorder + 5) {
-                    x -= speed;
-                    rect(x);
+                if (_settings.x >= _settings.leftBorder + 5) {
+                    _settings.x -= _settings.speed;
+                    draw(_settings.x);
                 }
-            },1000/frameRate)
+            },1000/_settings.frameRate)
         } 
     }
 }
