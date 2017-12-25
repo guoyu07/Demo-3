f09d8f0602bf69ee83b365ffd2613fd7900630af
Green <imgreenwu@gamil.com>
2017-12-25 19:21:02 +0800

    新增管理员更改项目信息功能

--- cat clicker/js/click.js
+++ cat clicker/js/click.js
@@ -1,34 +1,15 @@
  var log = console.log.bind(console);
-//面条式代码
-// function _click (list) {
-//     var show = document.getElementById("show");
-//     var title = document.querySelector('h4');
-//     var img = document.querySelector('img');
-//     var descText = document.querySelector('p');
-//     img.addEventListener("click", function(){
-//         list[img.id].likeCount +=1;
-//         descText.innerHTML = "这只猫被赞了"+list[img.id].likeCount+"次";
-//     })
-//     return function (list, i) {
-//         img.setAttribute("id",i);
-//         title.innerHTML = list[i].innerHTML;
-//         img.src = list[i].getAttribute("data-src");
-//         descText.innerHTML = "这只猫被赞了"+list[i].likeCount+"次";
-//     }
-// }
-// function __main(){
-// var list =document.querySelectorAll("h2")//载入所有图片资源
-// var click = _click(list);//注册click功能函数
-// for (var i=0; i<list.length; i++){
-//     (function (i){
-//         list[i].likeCount = 0;
-//         list[i].addEventListener("click", function(){click(list, i)} );
-//     })(i);
-// }//添加监听事件
-// }
-// __main();
-// 
+/**
+ * 数据交互模式为:
+ * 1.存储:先存储至 model, 后将 model 整体 stringfy 成 JSON 存储至 localstorage.
+ * 2.获取:直接从 localstorage 获取 JSON 数据并 parse 成对象使用.
+ */
+
 // (function(){
+
+/**
+ * 初始数据
+ */
 
 var model = {
     cat1: {
@@ -61,14 +42,23 @@ var model = {
         src:"https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1422921158,1241989086&fm=27&gp=0.jpg",
         likeCount:0,
     },
+    cat7: {
+        name:"七只鬼",
+        src:"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1514805455&di=c6fd072fed77976c2907ddd6180788af&imgtype=jpg&er=1&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201402%2F25%2F20140225171138_54uGn.thumb.600_0.jpeg",
+        likeCount:0,
+    },
 }
+
+/**
+ * localstorage 方法
+ */
 
 var storage = {
     init: function(){
         if(!localStorage.cat){
             localStorage.cat = JSON.stringify(model);//若第一次运行本程序则将 model 数据初始化载入 LocalStorage
         } else if (localStorage.cat) {
-            controller.LSinMod();//若非第一次运行则从 LocalStorage 获取点赞数据
+            controller.LSLoadToMod();//若非第一次运行则从 LocalStorage 获取点赞数据
         }
     },
     saveToLS: function(){
@@ -78,6 +68,10 @@ var storage = {
         return JSON.parse(localStorage.cat);//获取 LocalStorage 数据
     },
 }
+
+/**
+ * 集成控制器
+ */
 
 var controller = {
     init: function(){
@@ -93,15 +87,14 @@ var controller = {
     getCount: function(catId){
         return storage.getFromLS()["cat"+catId].likeCount;//获取点赞数据
     },
-    like: function(catId){
+    saveLike: function(catId){
         model["cat"+catId].likeCount += 1;//点赞+1s
         storage.saveToLS();//储存至 LocalStorage
-        view.updateLike(catId);// 在页面上更新点赞数
     },
-    LSinMod: function(){
+    LSLoadToMod: function(){
         model = storage.getFromLS();// model 从 LocalStorage 获取数据
     },
-    adminChangeLS: function(){
+    adminSaveChange: function(){
         var changingID = view.getStagePicID();//获得当前所修改的 ID
         var adminData = view.getAdminData();//载入表单数据
         for(var i in adminData){
@@ -110,12 +103,26 @@ var controller = {
             }
         }//遍历填写的内容,除去未填写的部分
         storage.saveToLS();
-        view.renderList();
-        view.renderPic(changingID);
-        view.updateLike(changingID);
+    },
+    renderList: function(){
+        var i,singleLi;
+        // var b =[];
+        var id = 0;
+        for(i in model){
+            singleLi = document.createElement("li");
+            id += 1;
+            singleLi.id = id;
+            singleLi.innerHTML = model[i].name;
+            view.list.appendChild(singleLi);
+            // b.push(singleLi);
+            // log(b.toString());
+        };
     },
 }
 
+/**
+ * 视图 DOM 相关的方法
+ */
 
 var view = {
     init: function(){
@@ -123,7 +130,7 @@ var view = {
         this.stage.s_title = document.querySelector('h4');
         this.stage.s_img = document.querySelector('img');
         this.stage.s_descText = document.querySelector('p');
-        this.list = document.querySelectorAll("h2");
+        this.list = document.querySelector("#list");
         this.adminBtn = document.querySelector("#admin-button");
         this.form = document.querySelector("#admin-form");
         this.formName = document.querySelector("#form-name");
@@ -131,24 +138,19 @@ var view = {
         this.formCount = document.querySelector("#form-count");
         this.formSave = document.querySelector("#form-save");
         this.formCount = document.querySelector("#form-count");
-
+        this.formCancel = document.querySelector("#form-cancel");
         view.renderList();
-        for (var i=0; i<view.list.length; i++){
-            (function(i){
-            view.list[i].addEventListener("click", function(e){view.setFlag(e);view.renderPic(i+1);view.updateLike(i+1)});//初始化遍历列表添加监听事件
-            })(i)
-        }
-        view.stage.s_img.addEventListener("click", function(e){controller.like(e.target.id)})//为展示区的图片添加点赞事件
-        view.adminBtn.addEventListener("click", view.toggleSwitch );
-        view.formSave.addEventListener("click",controller.adminChangeLS);
+        view.listAddEvent();
+        view.imgAddEvent();
+        view.adminBtnAddEvent();
+        view.saveBtnAddEvent();
+        view.cancelBtnAddEvent();
     },
     setFlag: function(e){
         view.stage.s_img.setAttribute("id",e.target.id);
     },
     renderList: function(){
-        for (var i = 0;i<view.list.length;i++){
-            view.list[i].innerHTML = controller.getName(i+1);
-        }
+        controller.renderList();
     },
     renderPic: function(catId){
         view.stage.s_title.innerHTML = controller.getName(catId);
@@ -174,6 +176,49 @@ var view = {
             likeCount: Number(view.formCount.value),
         }
         return a;
+    },
+    clearForm: function(){
+        view.formName.value = "";
+        view.formLink.value = "";
+        view.formCount.value = "";
+    },
+    clearList: function(){
+        view.list.innerHTML = "";
+    },
+    listAddEvent: function(){
+        for (var i=0; i<view.list.childNodes.length; i++){
+            (function(i){
+            view.list.childNodes[i].addEventListener("click", function(e){view.setFlag(e);view.renderPic(view.getStagePicID());view.updateLike(view.getStagePicID());})//初始化遍历列表添加监听事件
+            })(i)
+        }
+    },
+    imgAddEvent: function(){
+        view.stage.s_img.addEventListener("click", function(){
+            controller.saveLike(view.getStagePicID());
+            view.updateLike(view.getStagePicID());
+        })//为展示区的图片添加点赞事件
+    },
+    adminBtnAddEvent: function(){
+        view.adminBtn.addEventListener("click", function(){
+            view.toggleSwitch();view.clearForm();
+        });
+    },
+    saveBtnAddEvent: function(){
+        view.formSave.addEventListener("click",function(){
+            controller.adminSaveChange();
+            view.clearForm();
+            view.clearList();
+            view.renderList();
+            view.listAddEvent();
+            view.renderPic(view.getStagePicID());
+            view.updateLike(view.getStagePicID());
+            view.clearForm();
+        });
+    },
+    cancelBtnAddEvent: function(){
+        view.formCancel.addEventListener("click",function(){
+            view.toggleSwitch();view.clearForm();
+        });
     }
 }
 
