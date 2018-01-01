b74ca24e0fc88a448e127377555e2ded94179899
Green <imgreenwu@gamil.com>
2018-01-01 14:13:17 +0800

    添加自动隐藏的 sticky header

--- udacity/js/app.js
+++ udacity/js/app.js
@@ -1,30 +1,42 @@
 var log =console.log.bind(console);//方便调试
 
+// (function(){
 
-/**
- * 
- */
-
-(function(){
 var view = {
     init: function(){
         this.header = $(".header"),
         this.header_logo = document.querySelector(".header .logo img");
     },
-    isHeaderFixed : false,//优化性能
-    fixHeader: function(){
-        if(window.scrollY >= 50 && !view.isHeaderFixed){
-            view.isHeaderFixed = true;
-            view.header.addClass("header-sticky");
-            view.header_logo.src = "img/udacity-wordmark-cn.svg"
-        } else if (window.scrollY < 50 && view.isHeaderFixed){
-            view.isHeaderFixed = false;
-            view.header.removeClass("header-sticky");
-            view.header_logo.src = "img/udacity-wordmark-light-cn.svg"
+    cacheScroll : 0,
+    isScrollDownOrUp: function(){
+        var y = window.scrollY;
+        if (y > view.cacheScroll){
+            view.cacheScroll = y;
+            return true;// 向下滚动
+        } else if (y < view.cacheScroll) {
+            view.cacheScroll = y;
+            return false;// 向上滚动
         }
     },
+    isHeaderFixed : false,//优化性能
+    fixHeader: function(){ 
+        if (!view.isHeaderFixed && window.scrollY >= 60){
+            view.isHeaderFixed = true;
+            view.header.addClass("header-sticky-conceal");
+            view.header_logo.src = "img/udacity-wordmark-cn.svg"
+        } else if (view.isHeaderFixed && window.scrollY < 60 ){
+            view.isHeaderFixed = false;
+            view.header.removeClass("header-sticky-show");
+            view.header.removeClass("header-sticky-conceal");
+            view.header_logo.src = "img/udacity-wordmark-light-cn.svg"
+        } else if (view.isHeaderFixed && view.isScrollDownOrUp() ){
+            view.header.removeClass("header-sticky-show");
+        } 
+        else if (view.isHeaderFixed && !view.isScrollDownOrUp() && window.scrollY >= 100){
+            view.header.addClass("header-sticky-show");
+        } 
+    },
 }
-
 view.init();
-window.addEventListener("scroll", view.fixHeader);
-})();
+window.addEventListener("scroll", function(){view.fixHeader();});
+// })();
